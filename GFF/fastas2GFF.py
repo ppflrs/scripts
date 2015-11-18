07eb83fcb126a2b2254214bb85c70fd3beda6d2b
ppflrs <jfloresu@gmail.com>
2015-11-18 16:04:10 +0200

    Add parse of command line arguments
    - Now the script uses argv to receive the fasta file.
    - Passed argument defines input and output files.

--- GFF/fastas2GFF.py
+++ GFF/fastas2GFF.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 #fastas2GFF.py gets a multisequence FASTA file and outputs a GFF file
+import sys
+import os
 from Bio import SeqIO
 from Bio.Seq import Seq
 
-def map_bowtie(f, index_name):
-    handle = open(f, 'rU')
+if len(sys.argv) != 3:
+    sys.exit('Usage: %s <fasta_file> <bt2_index_name>' % sys.argv[0])
+
+def map_bowtie(input_file, index_name):
+    handle = open(input_file, 'rU')
     dna = list(SeqIO.parse(handle, 'fasta'))
     handle.close()
     map_dict = {}
     counter = 1 #To comply with the GFF format file specifications needs to start at 1
-    cat_genes = open(f + '.seq', 'a')
+
+    output_fasta_file = os.path.splitext(input_file)[0] + '.concatenated.fasta'
+    cat_genes = open(output_fasta_file, 'a')
     cat_genes.write('>' + index_name + '\n') #FASTA header
 
     seqid_counter = 1 #To order the output of the sequence
@@ -34,7 +41,13 @@ def map_bowtie(f, index_name):
         counter += (gene_len)
         cat_genes.write(str(sequence.seq))
 
-    with open(f + '.map', 'w') as m:
+    output_gff_file = os.path.splitext(input_file)[0] + '.GFF2'
+    with open(output_gff_file, 'w') as m:
         m.write('##gff-version 2\n')
         m.writelines('\t'.join(map(str,map_dict[k])) for k in map_dict.keys())
     return
+
+fasta_file = sys.argv[1]
+bt2_index = sys.argv[2]
+
+map_bowtie(fasta_file, bt2_index)
